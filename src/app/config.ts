1824110ab7187fb87ed2146fa5dfb122b17cc7ca
Guillaume <gmonnet@gmail.com>
2018-06-19 22:28:19 +0200

    Correct json templating link

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,6 +1,6 @@
 export const Config = {
   feedbackLink: 'https://github.com/255kb/mockoon/issues',
-  wikiJSONLink: 'https://github.com/255kb/mockoon/wiki/JSON-templating',
+  wikiJSONLink: 'https://github.com/255kb/mockoon/wiki/%7B-%7D-JSON-templating',
   updatesUrl: 'https://releases.mockoon.com/updates.json',
   releasesUrl: 'https://releases.mockoon.com/',
   version: '0.4.0'
