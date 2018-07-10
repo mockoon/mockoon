449eb90175a542d7ca5b0bf0773cbe7a1ed1937c
Guillaume <gmonnet@gmail.com>
2018-07-10 23:15:32 +0200

    Add proxy documentation

--- src/app/config.ts
+++ src/app/config.ts
@@ -5,6 +5,7 @@ export const Config = {
   version: '0.4.0',
   wikiLinks: {
     json: 'https://github.com/255kb/mockoon/wiki/%7B-%7D-JSON-templating',
+    proxy: 'https://github.com/255kb/mockoon/wiki/%F0%9F%9B%A1-Proxy-mode',
     cors: 'https://github.com/255kb/mockoon/wiki/%E2%9C%88-CORS-preflight-requests'
   }
 };
