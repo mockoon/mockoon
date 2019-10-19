24f1b46a6a3b4e17d2c99165e00db9f154d03716
Gabriel Peres Leopoldino <gabrielperes97@gmail.com>
2019-10-19 17:28:08 -0300

    Writing tests and a style correction

---
+++ test/proxy.spec.ts
@@ -0,0 +1,59 @@
+import { Tests } from './lib/tests';
+import { HttpCall } from './lib/types';
+
+const tests = new Tests('proxy');
+
+const getAnswerCall: HttpCall = {
+  description: 'Call GET answer',
+  path: '/answer',
+  method: 'GET',
+  testedProperties: {
+    body: '42',
+    status: 200
+  }
+};
+
+const environmentLogsItemSelector = '.environment-logs-column:nth-child(1) .menu-list .nav-item';
+
+describe('Proxy', () => {
+  tests.runHooks();
+
+  it('Start environments', async () => {
+    await tests.helpers.startEnvironment();
+    await tests.helpers.selectEnvironment(2);
+    await tests.helpers.startEnvironment();
+  });
+
+  it ('Call /anwser', () => {
+    tests.helpers.httpCallAsserterWithPort(getAnswerCall, 3001);
+  });
+
+  it('Environment logs have one entry', async () => {
+    await tests.helpers.switchViewInHeader('ENV_LOGS');
+    await tests.helpers.countEnvironmentLogsEntries(1);
+  });
+
+  it('First entry is GET /answer and was proxied by the application', async () => {
+    await tests.spectron.client.getText(`${environmentLogsItemSelector}:nth-child(1) .nav-link .route`).should.eventually.equal('GET\n/answer');
+    await tests.spectron.client.waitForExist(`${environmentLogsItemSelector}:nth-child(1) .nav-link i[ngbTooltip="Request proxied"]`, 5000, false);
+  });
+
+  it('Click on mock button ', async () => {
+    await tests.spectron.client.element(`${environmentLogsItemSelector}:nth-child(1) .btn-mock`).click();
+    await tests.helpers.stopEnvironment();
+    await tests.helpers.startEnvironment();
+  });
+
+  it('Check route added', async () => {
+    await tests.helpers.countRoutes(1);
+  });
+
+  it('Test new mock', async () => {
+    await tests.helpers.httpCallAsserterWithPort(getAnswerCall, 3001);
+    await tests.helpers.switchViewInHeader('ENV_LOGS');
+    await tests.helpers.countEnvironmentLogsEntries(2);
+    await tests.spectron.client.getText(`${environmentLogsItemSelector}:nth-child(1) .nav-link .route`).should.eventually.equal('GET\n/answer');
+    await tests.spectron.client.waitForExist(`${environmentLogsItemSelector}:nth-child(1) .nav-link i[ngbTooltip="Request proxied"]`, 5000, true);
+  });
+
+});
