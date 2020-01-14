2434fb7ffc81884c88428e7472432d41518d1d6f
Guillaume <hi@255kb.com>
2020-01-14 23:42:59 +0100

    Add tests for templating

--- test/lib/fetch.ts
+++ test/lib/fetch.ts
@@ -1,33 +1,48 @@
 export const fetch = (params: {
-  protocol: 'http' | 'https',
-  port: number, path: string,
-  method: 'GET' | 'POST' | 'PUT' | 'HEAD' | 'OPTIONS',
-  headers: { [key in string]: string },
-  body: any
+  protocol: 'http' | 'https';
+  port: number;
+  path: string;
+  method: 'GET' | 'POST' | 'PUT' | 'HEAD' | 'OPTIONS';
+  headers: { [key in string]: string };
+  body: any;
+  cookie: string;
 }) => {
   const data = JSON.stringify(params.body || {});
 
   return new Promise((resolve, reject) => {
-    const request = require(params.protocol).request({
-      hostname: `localhost`,
-      port: params.port,
-      path: params.path,
-      method: params.method.toUpperCase(),
-      headers: {
-        ...params.headers,
-        'Content-Length': data.length
-      }
-    }, (response) => {
-      let body = '';
-      response.on('data', (chunk) => body += chunk);
-      response.on('end', () => resolve({
-        status: response.statusCode,
-        headers: response.headers,
-        body
-      }));
-    });
+    const headers = {
+      ...params.headers,
+      'Content-Length': data.length
+    };
 
-    request.on('error', (err) => reject(err));
+    if (params.cookie) {
+      headers['Cookie'] = `${params.cookie}; expires=${new Date(
+        new Date().getTime() + 86409000
+      )}`;
+    }
+
+    const request = require(params.protocol).request(
+      {
+        hostname: `localhost`,
+        port: params.port,
+        path: params.path,
+        method: params.method.toUpperCase(),
+        headers
+      },
+      response => {
+        let body = '';
+        response.on('data', chunk => (body += chunk));
+        response.on('end', () =>
+          resolve({
+            status: response.statusCode,
+            headers: response.headers,
+            body
+          })
+        );
+      }
+    );
+
+    request.on('error', err => reject(err));
 
     request.write(data);
     request.end();
--- test/lib/types.ts
+++ test/lib/types.ts
@@ -3,6 +3,7 @@ export type HttpCall = {
   path: string;
   method: 'GET' | 'POST' | 'PUT' | 'HEAD' | 'OPTIONS';
   headers?: { [key in string]: string };
+  cookie?: string,
   body?: any;
   testedProperties: {
     body?: string;
