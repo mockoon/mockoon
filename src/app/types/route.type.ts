c42d366942d55b7e4957e790cb57925ae682288e
Guillaume <hi@255kb.com>
2020-04-04 10:22:23 +0200

    Add missing status code 418
    Closes  #270

--- src/app/types/route.type.ts
+++ src/app/types/route.type.ts
@@ -50,6 +50,7 @@ export const statusCodes = [
   { code: 415, text: 'Unsupported Media Type' },
   { code: 416, text: 'Range Not Satisfiable' },
   { code: 417, text: 'Expectation Failed' },
+  { code: 418, text: 'I\'m a teapot' },
   { code: 421, text: 'Misdirected Request' },
   { code: 422, text: 'Unprocessable Entity' },
   { code: 423, text: 'Locked' },
