09231b3ca6cc210aefe5fe8c6f51f4244d891349
Guillaume <hi@255kb.dev>
2021-02-09 22:13:01 +0100

    Allow responsive images in changelog
    Closes #417

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { HttpClientModule } from '@angular/common/http';
-import { ErrorHandler, NgModule } from '@angular/core';
+import { ErrorHandler, NgModule, SecurityContext } from '@angular/core';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireRemoteConfigModule } from '@angular/fire/remote-config';
@@ -64,6 +64,7 @@ import { AppComponent } from './app.component';
     HttpClientModule,
     NgbModule,
     MarkdownModule.forRoot({
+      sanitize: SecurityContext.NONE,
       markedOptions: {
         provide: MarkedOptions,
         useFactory: MarkedOptionsFactory
