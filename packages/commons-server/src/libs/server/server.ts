31a87bd0798927fc723268212de3e0aa4c4dda74
Guillaume <hi@255kb.dev>
2022-05-17 13:55:25 +0200

    Fix prefix not being removed when proxying to a prefixed endpoint (#710)
    Closes #585

--- packages/commons-server/src/libs/server/server.ts
+++ packages/commons-server/src/libs/server/server.ts
@@ -579,6 +579,18 @@ export class MockoonServer extends (EventEmitter as new () => TypedEmitter<Serve
           secure: false,
           changeOrigin: true,
           logProvider: this.options.logProvider,
+          pathRewrite: (path, req) => {
+            if (
+              this.environment.proxyRemovePrefix === true &&
+              this.environment.endpointPrefix.length > 0
+            ) {
+              const regExp = new RegExp(`^/${this.environment.endpointPrefix}`);
+
+              return path.replace(regExp, '');
+            }
+
+            return path;
+          },
           ssl: { ...this.tlsOptions, agent: false },
           onProxyReq: (proxyReq, request, response) => {
             this.refreshEnvironment();
@@ -590,14 +602,6 @@ export class MockoonServer extends (EventEmitter as new () => TypedEmitter<Serve
               proxyReq,
               request as Request
             );
-
-            if (
-              this.environment.proxyRemovePrefix === true &&
-              this.environment.endpointPrefix.length > 0
-            ) {
-              const regExp = new RegExp(`^/${this.environment.endpointPrefix}`);
-              proxyReq.path = proxyReq.path.replace(regExp, '');
-            }
 
             // re-stream the body (intercepted by body parser method)
             if (request.rawBody) {
