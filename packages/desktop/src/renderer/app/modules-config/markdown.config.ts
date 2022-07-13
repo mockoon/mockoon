b2e0aae74ddb4d6a3bec9ea7f96ef8a6ed87f25a
Guillaume <hi@255kb.dev>
2022-07-13 10:52:43 +0200

    Make the release note links clickable

--- packages/desktop/src/renderer/app/modules-config/markdown.config.ts
+++ packages/desktop/src/renderer/app/modules-config/markdown.config.ts
@@ -3,14 +3,9 @@ import { MarkedOptions, MarkedRenderer } from 'ngx-markdown';
 export const MarkedOptionsFactory = (): MarkedOptions => {
   const renderer = new MarkedRenderer();
 
-  // Whitelist links domains
-  renderer.link = (href: string, title: string, text: string) => {
-    if (href.match(/mockoon\.com|github\.com/i)) {
-      return `<a href="openexternal::${href}" target="_blank">${text}</a>`;
-    }
-
-    return text;
-  };
+  // Open all links in an external browser
+  renderer.link = (href: string, title: string, text: string) =>
+    `<a href="openexternal::${href}" target="_blank">${text}</a>`;
 
   // Make images responsive
   renderer.image = (href: string, title: string, text: string) =>
