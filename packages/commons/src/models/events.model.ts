7e03b3ab5a74047aea12e52e3d87d6bee816dd1f
Guillaume <hi@255kb.dev>
2022-07-22 09:58:07 +0200

    Update dependencies (#763)

--- packages/commons/src/models/events.model.ts
+++ packages/commons/src/models/events.model.ts
@@ -1,11 +1,11 @@
 import { ServerErrorCodes } from '../enums/errors.enum';
 import { Transaction } from './server.model';
 
-export interface ServerEvents {
+export type ServerEvents = {
   error: (errorCode: ServerErrorCodes, originalError?: Error) => void;
   stopped: () => void;
   'creating-proxy': () => void;
   started: () => void;
   'entering-request': () => void;
   'transaction-complete': (transaction: Transaction) => void;
-}
+};
