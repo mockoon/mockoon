ccc3274b5dbd0f97adccb6ab85c72ce53eed8d91
Martin <7595909+martin-bucinskas@users.noreply.github.com>
2023-04-23 19:10:06 +0100

    feat(cli): fixing bug on windows with logging to /dev/null
    - logging on windows to  will create a file null in  directory
    
    Closes #868

--- packages/cli/src/commands/start.ts
+++ packages/cli/src/commands/start.ts
@@ -168,9 +168,9 @@ export default class Start extends Command {
         ? environmentInfo.initialDataDir
         : '',
       logTransaction: environmentInfo.logTransaction,
-      fileTransportsOptions: [
-        { filename: environmentInfo.disableLogToFile ? '/dev/null' : join(Config.logsPath, `${environmentInfo.name}-out.log`) }
-      ]
+      fileTransportsOptions: (environmentInfo.disableLogToFile ? [] : [{
+        filename: join(Config.logsPath, `${environmentInfo.name}-out.log`)}
+      ])
     };
 
     createServer(parameters);
@@ -198,8 +198,8 @@ export default class Start extends Command {
       min_uptime: 10000,
       kill_timeout: 2000,
       args,
-      error: environmentInfo.disableLogToFile ? '/dev/null' : join(Config.logsPath, `${environmentInfo.name}-error.log`),
-      output: environmentInfo.disableLogToFile ? '/dev/null' : join(Config.logsPath, `${environmentInfo.name}-out.log`),
+      error: environmentInfo.disableLogToFile ? undefined : join(Config.logsPath, `${environmentInfo.name}-error.log`),
+      output: environmentInfo.disableLogToFile ? undefined : join(Config.logsPath, `${environmentInfo.name}-out.log`),
       name: environmentInfo.name,
       script: resolve(__dirname, '../libs/server-start-script.js'),
       exec_mode: 'fork'
