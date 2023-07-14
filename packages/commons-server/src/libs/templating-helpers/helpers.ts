2f6cc4fc5a419d8ee85e0fd2ff30da505a26689c
Guillaume <guillaume@1kb.software>
2023-07-14 15:39:41 +0200

    Add stringify option to oneof helper

--- packages/commons-server/src/libs/templating-helpers/helpers.ts
+++ packages/commons-server/src/libs/templating-helpers/helpers.ts
@@ -67,8 +67,26 @@ export const Helpers = {
     return content;
   },
   // return one random item
-  oneOf: function (itemList: string[]) {
-    return itemList[RandomInt(0, itemList.length - 1)];
+  oneOf: function (...args: any[]) {
+    const parameters = args.slice(0, -1);
+    let stringify = false;
+    let itemList: any[] = [];
+
+    if (parameters.length === 0 || !Array.isArray(parameters[0])) {
+      return '';
+    }
+
+    if (parameters.length >= 1) {
+      itemList = parameters[0] || [];
+    }
+
+    if (parameters.length >= 2) {
+      stringify = parameters[1];
+    }
+
+    const result = itemList[RandomInt(0, itemList.length - 1)];
+
+    return stringify ? new SafeString(JSON.stringify(result)) : result;
   },
   // return some random item as an array (to be used in triple braces) or as a string
   someOf: function (
