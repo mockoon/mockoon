8c18fa8ee8f27e40afc921d5b4665c2ec129bc66
ed.t <editied@gmail.com>
2023-08-17 22:52:34 +0200

    [Feature Request] - Issue 1103 Add helper object Add helper filter

--- packages/commons-server/src/libs/templating-helpers/helpers.ts
+++ packages/commons-server/src/libs/templating-helpers/helpers.ts
@@ -874,5 +874,61 @@ export const Helpers = {
     }
 
     return value.padEnd(length, padChar);
+  },
+  object: function (options: HelperOptions) {
+    return options.hash;
+  },
+  filter: function (...args) {
+    const parameters = args.slice(0, -1);
+
+    if (parameters.length === 0) {
+      return '';
+    }
+
+    const [arr, ...filters] = parameters;
+
+    if (!(arr instanceof Array)) {
+      return '';
+    }
+    if (!arr.length || !filters.length) {
+      return arr;
+    }
+
+    const validate = (payload, condition) => {
+      if (
+        condition !== null &&
+        typeof condition === 'object' &&
+        !Array.isArray(condition)
+      ) {
+        if (typeof payload === 'object') {
+          const keys = Object.keys(condition);
+
+          return keys.every((k) => payload[k] === condition[k]);
+        }
+
+        return false;
+      }
+
+      return payload === condition;
+    };
+
+    const and = (item, conditions, { or }) =>
+      conditions.every((subConditions) => {
+        if (Array.isArray(subConditions)) {
+          return or(item, subConditions);
+        }
+
+        return validate(item, subConditions);
+      });
+    const or = (item, conditions) =>
+      conditions.some((subConditions) => {
+        if (Array.isArray(subConditions)) {
+          return and(item, subConditions, { or });
+        }
+
+        return validate(item, subConditions);
+      });
+
+    return arr.filter((item) => or(item, filters));
   }
 };
