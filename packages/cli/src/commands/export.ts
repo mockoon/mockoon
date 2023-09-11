d08714f236881d6f11eb2e1f844f9769ccea1d45
Matheus Braz <18542083+mavbraz@users.noreply.github.com>
2023-09-10 21:51:36 -0300

    Adapt to new release changes

--- packages/cli/src/commands/export.ts
+++ packages/cli/src/commands/export.ts
@@ -1,38 +1,38 @@
 import { OpenAPIConverter } from '@mockoon/commons-server';
-import { Command, flags } from '@oclif/command';
+import { Command, Flags } from '@oclif/core';
 import { promises as fs } from 'fs';
 import { parseDataFiles } from '../libs/data';
 
 export default class Export extends Command {
   public static description = 'Export a mock API';
+
   public static examples = [
     '$ mockoon-cli export --data ~/data.json --output ./output.json',
     '$ mockoon-cli export --data ~/data.json --output ./output.json --type open-api-v3'
   ];
 
   public static flags = {
-    data: flags.string({
+    data: Flags.string({
       char: 'd',
       description: 'Path or URL to your Mockoon data file',
       required: true
     }),
-    output: flags.string({
+    output: Flags.string({
       char: 'o',
       description: 'Generated OpenApi v3 path and name (e.g. `./output.json`)',
       required: true
     }),
-    type: flags.string({
+    type: Flags.string({
       char: 't',
       description: 'Export type',
       options: ['open-api-v3'],
-      parse: (input) => input.toLowerCase(),
       default: 'open-api-v3',
       required: false
     })
   };
 
   public async run(): Promise<void> {
-    const { flags: userFlags } = this.parse(Export);
+    const { flags: userFlags } = await this.parse(Export);
 
     try {
       const parsedEnvironments = await parseDataFiles([userFlags.data]);
@@ -42,7 +42,7 @@ export default class Export extends Command {
         default:
           const openApiConverter = new OpenAPIConverter();
           data = await openApiConverter.convertToOpenAPIV3(
-            parsedEnvironments.environments[0]
+            parsedEnvironments[0].environment
           );
       }
 
--- packages/cli/src/commands/import.ts
+++ packages/cli/src/commands/import.ts
@@ -1,53 +1,54 @@
-import { Command, flags } from '@oclif/command';
+import { Command, Flags } from '@oclif/core';
 import { promises as fs } from 'fs';
 import { CLIMessages } from '../constants/cli-messages.constants';
 import { parseDataFiles } from '../libs/data';
 
 export default class Import extends Command {
   public static description = 'Import a mock API';
+
   public static examples = [
     '$ mockoon-cli import --data ~/data.json --output ./output.json',
     '$ mockoon-cli import --data ~/data.json --output ./output.json --type open-api-v3'
   ];
 
   public static flags = {
-    data: flags.string({
+    data: Flags.string({
       char: 'd',
       description: 'Path or URL to your data file',
       required: true
     }),
-    output: flags.string({
+    output: Flags.string({
       char: 'o',
       description:
         'Generated Mockoon path and name (e.g. `./environment.json`)',
       required: true
     }),
-    port: flags.integer({
+    port: Flags.integer({
       char: 'p',
       description: 'Environment port',
       default: 8080
     }),
-    prettify: flags.boolean({
+    prettify: Flags.boolean({
       description: 'Prettify output',
       default: false
     })
   };
 
   public async run(): Promise<void> {
-    const { flags: userFlags } = this.parse(Import);
+    const { flags: userFlags } = await this.parse(Import);
 
     try {
       const parsedEnvironments = await parseDataFiles([userFlags.data]);
 
-      if (parsedEnvironments.environments.length !== 1) {
+      if (parsedEnvironments.length !== 1) {
         this.error(CLIMessages.ONLY_ONE_ENVIRONMENT_ALLOWED);
       }
 
-      const environment = parsedEnvironments.environments[0];
+      const environment = parsedEnvironments[0].environment;
       environment.port = userFlags.port;
 
       const data = JSON.stringify(
-        parsedEnvironments.environments[0],
+        environment,
         null,
         userFlags.prettify ? 2 : 0
       );
