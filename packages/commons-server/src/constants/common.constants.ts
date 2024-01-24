7d22ea06ba843981af2a667ec373fe3ed40e4517
Guillaume <guillaume@mockoon.com>
2024-01-24 17:13:36 +0100

    Add support for vendor specific JSON content types
    Some clients uses vendor specific JSON Content-Type like `application/vnd.xxxxxxx+json`. This add support for these types in the request body parsing and rules interpreter.
    Closes #1269

--- packages/commons-server/src/constants/common.constants.ts
+++ packages/commons-server/src/constants/common.constants.ts
@@ -4,8 +4,13 @@ export const ParsedXMLBodyMimeTypes = [
   'text/xml'
 ];
 
-export const ParsedBodyMimeTypes = [
+export const ParsedJSONBodyMimeTypes = [
   'application/json',
+  /application\/.*\+json/i
+];
+
+export const ParsedBodyMimeTypes = [
+  ...ParsedJSONBodyMimeTypes,
   'application/x-www-form-urlencoded',
   'multipart/form-data',
   ...ParsedXMLBodyMimeTypes
