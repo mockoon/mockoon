8c08358d3c5f541e6bf3116f72d268c4a72bdf30
Guillaume <guillaume@mockoon.com>
2024-08-01 10:50:34 +0200

    Add jwtPayload and jwtHeader helpers (#1475)
    Closes #774

---
+++ packages/commons-server/src/libs/templating-helpers/helpers/jwt.ts
@@ -0,0 +1,47 @@
+import { fromSafeString } from '../../utils';
+
+const buildJwtHelper = (partIndex: 0 | 1) =>
+  function (...args: any[]) {
+    // remove last item (handlebars options argument)
+    const parameters = args.slice(0, -1);
+
+    if (parameters.length < 1) {
+      return;
+    }
+
+    let jwt = fromSafeString(parameters[0]);
+    const key = fromSafeString(parameters[1]);
+
+    if (typeof jwt !== 'string') {
+      return;
+    }
+
+    // Remove the 'Bearer ' prefix if present
+    jwt = jwt.replace(/^Bearer /, '');
+
+    try {
+      const payload = JSON.parse(
+        Buffer.from(jwt.split('.')[partIndex], 'base64').toString('utf-8')
+      );
+
+      return key !== undefined ? payload[key] : payload;
+    } catch (e) {
+      return;
+    }
+  };
+
+/**
+ * Returns a value from a JWT header
+ *
+ * @param args
+ * @returns
+ */
+export const jwtHeader = buildJwtHelper(0);
+
+/**
+ * Returns a value from a JWT payload
+ *
+ * @param args
+ * @returns
+ */
+export const jwtPayload = buildJwtHelper(1);
