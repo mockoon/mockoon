91cc4c62b34c288d94e59917d7045351bbfa7f34
Guillaume <guillaume@mockoon.com>
2024-08-27 14:28:16 +0200

    Update stringify helper to be compatible with SafeString (#1490)
    Closes #1489

--- packages/commons-server/src/libs/templating-helpers/helpers/stringify.ts
+++ packages/commons-server/src/libs/templating-helpers/helpers/stringify.ts
@@ -1,11 +1,17 @@
 // Returns Objects as formatted JSON String
-import { HelperOptions } from 'handlebars';
+import { HelperOptions, SafeString } from 'handlebars';
+import { fromSafeString } from '../../utils';
 
 const stringify = function (data: unknown, options: HelperOptions) {
   if (!options) {
     return;
   }
+
   if (data && typeof data === 'object') {
+    if (data instanceof SafeString) {
+      data = fromSafeString(data);
+    }
+
     return JSON.stringify(data, null, 2);
   } else {
     return data;
