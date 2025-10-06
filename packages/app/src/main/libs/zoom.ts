baf996dcff8ea0b50ae5d0429fb3247247f91eed
Guillaume <guillaume@mockoon.com>
2025-10-06 15:26:48 +0200

    Add more zoom levels in the desktop application
    Closes #1842

--- packages/app/src/main/libs/zoom.ts
+++ packages/app/src/main/libs/zoom.ts
@@ -1,16 +1,21 @@
 import { BrowserWindow, Menu } from 'electron';
 
+const defaultZoomFactor = 1;
+const maxZoomFactor = 1.6;
+const minZoomFactor = 0.8;
+const zoomStep = 0.1;
+
 export const handleZoomIn = (mainWindow: BrowserWindow) => {
   const menuInstance = Menu.getApplicationMenu();
   menuInstance.getMenuItemById('MENU_ZOOM_OUT').enabled = true;
 
-  if (mainWindow.webContents.zoomFactor >= 1.3) {
+  if (mainWindow.webContents.zoomFactor >= maxZoomFactor) {
     return;
   }
 
-  mainWindow.webContents.zoomFactor += 0.1;
+  mainWindow.webContents.zoomFactor += zoomStep;
 
-  if (mainWindow.webContents.zoomFactor >= 1.3) {
+  if (mainWindow.webContents.zoomFactor >= maxZoomFactor) {
     menuInstance.getMenuItemById('MENU_ZOOM_IN').enabled = false;
   }
 };
@@ -19,13 +24,13 @@ export const handleZoomOut = (mainWindow: BrowserWindow) => {
   const menuInstance = Menu.getApplicationMenu();
   menuInstance.getMenuItemById('MENU_ZOOM_IN').enabled = true;
 
-  if (mainWindow.webContents.zoomFactor <= 0.8) {
+  if (mainWindow.webContents.zoomFactor <= minZoomFactor) {
     return;
   }
 
-  mainWindow.webContents.zoomFactor -= 0.1;
+  mainWindow.webContents.zoomFactor -= zoomStep;
 
-  if (mainWindow.webContents.zoomFactor <= 0.8) {
+  if (mainWindow.webContents.zoomFactor <= minZoomFactor) {
     menuInstance.getMenuItemById('MENU_ZOOM_OUT').enabled = false;
   }
 };
@@ -35,5 +40,5 @@ export const handleZoomReset = (mainWindow: BrowserWindow) => {
   menuInstance.getMenuItemById('MENU_ZOOM_IN').enabled = true;
   menuInstance.getMenuItemById('MENU_ZOOM_OUT').enabled = true;
 
-  mainWindow.webContents.zoomFactor = 1;
+  mainWindow.webContents.zoomFactor = defaultZoomFactor;
 };
