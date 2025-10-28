826ab49c58b14258b1ef7374bbfabfa3af5c2c09
Guillaume <guillaume@mockoon.com>
2025-10-28 11:18:36 +0100

    Update dependencies and some Angular config
    See #1824

--- packages/app/src/renderer/polyfills.ts
+++
@@ -1,5 +0,0 @@
-/** *************************************************************************************************
- * Load `$localize` onto the global scope - used if i18n tags appear in Angular templates.
- */
-import '@angular/localize/init';
-import 'zone.js';
