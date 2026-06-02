d77bfdb2a14e0420295913ed2824c07d35f65ef8
Guillaume <guillaume@mockoon.com>
2026-06-02 22:13:32 +0200

    Simplify duplicate modal code

--- packages/app/src/renderer/app/components/modals/duplicate-modal/duplicate-modal.component.ts
+++ packages/app/src/renderer/app/components/modals/duplicate-modal/duplicate-modal.component.ts
@@ -1,15 +1,8 @@
 import { AsyncPipe } from '@angular/common';
-import {
-  ChangeDetectionStrategy,
-  Component,
-  inject,
-  OnDestroy,
-  OnInit
-} from '@angular/core';
-import { Callback, DataBucket, Environment, Route } from '@mockoon/commons';
-import { combineLatest, Observable, Subscription } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
-import { DataSubject } from 'src/renderer/app/models/data.model';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { Environment } from '@mockoon/commons';
+import { combineLatest, Observable } from 'rxjs';
+import { filter, map, tap } from 'rxjs/operators';
 import { DuplicateEntityToAnotherEnvironment } from 'src/renderer/app/models/store.model';
 import { EnvironmentsService } from 'src/renderer/app/services/environments.service';
 import { UIService } from 'src/renderer/app/services/ui.service';
@@ -22,7 +15,7 @@ import { Store } from 'src/renderer/app/stores/store';
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [AsyncPipe]
 })
-export class DuplicateModalComponent implements OnInit, OnDestroy {
+export class DuplicateModalComponent {
   private uiService = inject(UIService);
   private store = inject(Store);
   private environmentsService = inject(EnvironmentsService);
@@ -57,39 +50,20 @@ export class DuplicateModalComponent implements OnInit, OnDestroy {
         )
     )
   );
-  public entityInformation: {
-    displayName: string;
-    subject: Omit<DataSubject, 'environment'>;
-    uuids: string[];
-  } = {
-    displayName: '',
-    subject: '',
-    uuids: []
-  };
-
-  private entityDuplicationState$ = this.store.select(
-    'duplicateEntityToAnotherEnvironment'
-  );
-
-  private entityDuplicationSubscription: Subscription;
-
-  private get activeEnvironment() {
-    return this.store.getActiveEnvironment();
-  }
-
-  ngOnInit() {
-    this.entityDuplicationSubscription = this.entityDuplicationState$.subscribe(
-      (state: DuplicateEntityToAnotherEnvironment) => {
+  public entityDuplicationState$ = this.store
+    .select('duplicateEntityToAnotherEnvironment')
+    .pipe(
+      tap((state: DuplicateEntityToAnotherEnvironment) => {
         if (state.moving) {
-          this.extractEntityToDuplicate(state);
+          if (!state.subjectUuids || state.subjectUuids.length === 0) {
+            this.store.update(
+              cancelEntityDuplicationToAnotherEnvironmentAction()
+            );
+            this.uiService.closeModal('duplicate_to_environment', false);
+          }
         }
-      }
+      })
     );
-  }
-
-  ngOnDestroy() {
-    this.entityDuplicationSubscription.unsubscribe();
-  }
 
   public close() {
     this.uiService.closeModal('duplicate_to_environment', false);
@@ -97,24 +71,20 @@ export class DuplicateModalComponent implements OnInit, OnDestroy {
 
   public chooseTargetEnvironment(
     targetEnvironment: Environment,
-    entityInformation: {
-      displayName: string;
-      subject: Omit<DataSubject, 'environment'>;
-      uuids: string[];
-    }
+    state: DuplicateEntityToAnotherEnvironment
   ) {
-    for (const uuid of entityInformation.uuids) {
-      if (entityInformation.subject === 'route') {
+    for (const uuid of state.subjectUuids) {
+      if (state.subject === 'route') {
         this.environmentsService.duplicateRouteInAnotherEnvironment(
           uuid,
           targetEnvironment.uuid
         );
-      } else if (entityInformation.subject === 'databucket') {
+      } else if (state.subject === 'databucket') {
         this.environmentsService.duplicateDatabucketInAnotherEnvironment(
           uuid,
           targetEnvironment.uuid
         );
-      } else if (entityInformation.subject === 'callback') {
+      } else if (state.subject === 'callback') {
         this.environmentsService.duplicateCallbackInAnotherEnvironment(
           uuid,
           targetEnvironment.uuid
@@ -124,58 +94,5 @@ export class DuplicateModalComponent implements OnInit, OnDestroy {
 
     this.store.update(cancelEntityDuplicationToAnotherEnvironmentAction());
     this.uiService.closeModal('duplicate_to_environment');
-  }
-
-  private extractEntityToDuplicate(state: DuplicateEntityToAnotherEnvironment) {
-    const uuids = state.subjectUuids ?? [];
-
-    if (uuids.length === 0) {
-      this.store.update(cancelEntityDuplicationToAnotherEnvironmentAction());
-      this.uiService.closeModal('duplicate_to_environment', false);
-
-      return;
-    }
-
-    if (state.subject === 'route') {
-      // Batch case: multiple routes selected.
-      if (uuids.length > 1) {
-        this.entityInformation = {
-          displayName: `${uuids.length} routes`,
-          subject: state.subject,
-          uuids: [...uuids]
-        };
-
-        return;
-      }
-
-      const entityToDuplicate = this.activeEnvironment.routes.find(
-        (route: Route) => route.uuid === uuids[0]
-      );
-      this.entityInformation = {
-        displayName: `${entityToDuplicate.method.toUpperCase()} /${
-          entityToDuplicate.endpoint
-        }`,
-        subject: state.subject,
-        uuids: [entityToDuplicate.uuid]
-      };
-    } else if (state.subject === 'databucket') {
-      const entityToDuplicate = this.activeEnvironment.data.find(
-        (databucket: DataBucket) => databucket.uuid === uuids[0]
-      );
-      this.entityInformation = {
-        displayName: entityToDuplicate.name,
-        subject: state.subject,
-        uuids: [entityToDuplicate.uuid]
-      };
-    } else if (state.subject === 'callback') {
-      const entityToDuplicate = this.activeEnvironment.callbacks.find(
-        (cb: Callback) => cb.uuid === uuids[0]
-      );
-      this.entityInformation = {
-        displayName: entityToDuplicate.name,
-        subject: state.subject,
-        uuids: [entityToDuplicate.uuid]
-      };
-    }
   }
 }
--- packages/app/src/renderer/app/models/store.model.ts
+++ packages/app/src/renderer/app/models/store.model.ts
@@ -66,7 +66,7 @@ export type UIState = {
 
 export type DuplicateEntityToAnotherEnvironment = {
   moving: boolean;
-  subject?: Omit<DataSubject, 'environment'>;
+  subject?: Omit<DataSubject, 'environment' | 'folder'>;
   // UUIDs of the entities to duplicate. Always an array; contains a single
   // element for the standard single-entity flow, multiple for batch flows
   // (currently only supported for routes).
