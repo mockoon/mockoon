76eb4853efcdfc576f7e46a85e2511655a1c0d31
Guillaume <guillaume@mockoon.com>
2026-07-04 17:09:33 +0200

    Fix browser reload

--- packages/app/test/libs/file.ts
+++ packages/app/test/libs/file.ts
@@ -24,7 +24,7 @@ class File {
     );
     settings = { ...settings, ...properties };
     await fs.writeFile(this.settingsPath, JSON.stringify(settings));
-    await browser.reconnect('');
+    await browser.reloadSession();
     await browser.pause(2000);
   }
 
